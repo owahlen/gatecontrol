4f6f158dbe91a848ee8d0b9c7d325a74444faa01
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2021-09-27 20:35:23 +0200

    changed logging format to logging.BASIC_FORMAT

--- app/api/logger.py
+++ app/api/logger.py
@@ -3,6 +3,6 @@ import logging
 logger = logging.getLogger('gatecontrol')
 # Note that the log level of the logger is set in the Config
 ch = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(logging.BASIC_FORMAT)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
